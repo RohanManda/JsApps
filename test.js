876cdd25a5a9656e324449c41c46824e17b8296f
Rohan Manda <rohanmanda0@gmail.com>
2025-05-10 10:20:39 +0530

    third commit

--- test.js
+++ test.js
@@ -7,5 +7,5 @@ const server=http.createServer((req,res)=>{
     res.end('Hello World');
 });
 server.listen(port,()=>{
-console.log(`Server running at http://${hostname}:{port}/`);
+console.log(`Server running at http://${hostname}:${port}/`);
 });
